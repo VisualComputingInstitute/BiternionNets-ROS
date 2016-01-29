d8da58db122b4d8f854644b9543e51fc4968e675
yobibyte <vitaliykurin@gmail.com>
2016-01-29 16:09:48 +0100

    Updated play/dump script

--- scripts/playanddump.sh
+++ scripts/playanddump.sh
@@ -4,12 +4,15 @@
 rosparam set use_sim_time true
 
 for i in $( ls $1 | grep .bag); do
-  p=$(echo $i | sed 's/.bag//' | sed 's/.*\-//')
-  screen -m -d roslaunch biternion dump_tracks.launch dir:=/work/kurin/spencer_data/dump/$p hfactor:=0.5 wfactor:=0.8 src:=rear
+  roslaunch spencer_rwth all.launch sensor_frame:=rear &
   foo=$!
+  p=$(echo $i | sed 's/.bag//' | sed 's/.*\-//')
+  roslaunch biternion dump_tracks.launch dir:=/work/kurin/spencer_data/dump/$p hfactor:=0.5 wfactor:=0.8 src:=rear &
+  sleep 5
   rosbag play --clock -r 0.3 $1$i
   echo $foo
   kill $foo
+  sleep 5
 done
 
 
