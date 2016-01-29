ca8b39af430720cf06ed3e0ed39835aac1bc2684
yobibyte <vitaliykurin@gmail.com>
2016-01-29 16:10:37 +0100

    Changed mknet after DF2 refactoring

--- scripts/common.py
+++ scripts/common.py
@@ -5,24 +5,24 @@ from df_extras import Flatten, Biternion
 
 def mknet():
   return df.Sequential(                   #     3@46
-    df.SpatialConvolution( 3, 24, 3, 3),  # -> 24@44
+    df.SpatialConvolution( 3, 24, (3, 3)),  # -> 24@44
     df.BatchNormalization(24),
     df.ReLU(),
-    df.SpatialConvolution(24, 24, 3, 3),  # -> 24@42
+    df.SpatialConvolution(24, 24, (3, 3)),  # -> 24@42
     df.BatchNormalization(24),
-    df.SpatialMaxPooling(2, 2),           # -> 24@21
+    df.SpatialMaxPooling((2, 2)),           # -> 24@21
     df.ReLU(),
-    df.SpatialConvolution(24, 48, 3, 3),  # -> 48@19
+    df.SpatialConvolution(24, 48, (3, 3)),  # -> 48@19
     df.BatchNormalization(48),
     df.ReLU(),
-    df.SpatialConvolution(48, 48, 3, 3),  # -> 48@17
+    df.SpatialConvolution(48, 48, (3, 3)),  # -> 48@17
     df.BatchNormalization(48),
-    df.SpatialMaxPooling(2, 2),           # -> 48@9
+    df.SpatialMaxPooling((2, 2)),           # -> 48@9
     df.ReLU(),
-    df.SpatialConvolution(48, 64, 3, 3),  # -> 48@7
+    df.SpatialConvolution(48, 64, (3, 3)),  # -> 48@7
     df.BatchNormalization(64),
     df.ReLU(),
-    df.SpatialConvolution(64, 64, 3, 3),  # -> 48@5
+    df.SpatialConvolution(64, 64, (3, 3)),  # -> 48@5
     df.BatchNormalization(64),
     df.ReLU(),
     df.Dropout(0.2),
@@ -30,7 +30,7 @@ def mknet():
     df.Linear(64*5*5, 512),
     df.ReLU(),
     df.Dropout(0.5),
-    df.Linear(512, 2, initW=df.init.normal(0.01)),
+    df.Linear(512, 2, init=df.init.normal(0.01)),
     Biternion()
   )
 
