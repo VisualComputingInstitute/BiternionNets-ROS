9066a469f054537f3ccf476edf2ae653c9529cbe
yobibyte <vitaliykurin@gmail.com>
2016-02-18 11:55:15 +0100

    New dump file with bg subtraction

--- scripts/playanddump.sh
+++ scripts/playanddump.sh
@@ -7,7 +7,7 @@ for i in $( ls $1 | grep .bag); do
   roslaunch spencer_rwth all.launch sensor_frame:=rear &
   foo=$!
   p=$(echo $i | sed 's/.bag//' | sed 's/.*\-//')
-  roslaunch biternion dump_tracks.launch dir:=/work/kurin/spencer_data/dump/$p hfactor:=0.5 wfactor:=0.8 src:=rear &
+  roslaunch biternion dump_tracks.launch dir:=/work/kurin/spencer_data/dump/$p hfactor:=0.5 wfactor:=0.8 src:=rear subbg:=true &
   sleep 5
   rosbag play --clock -r 0.3 $1$i
   echo $foo
