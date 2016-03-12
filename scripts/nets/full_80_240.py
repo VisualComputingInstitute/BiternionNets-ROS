820dc97191c515d7bbede1c6c7b3512c511ffe72
lucasb-eyer <lucasb.eyer.be@gmail.com>
2016-03-12 01:57:29 +0100

    First try at a full-body network.

---
+++ scripts/nets/full_80_240.py
@@ -0,0 +1,43 @@
+import DeepFried2 as df
+from lbtoolbox.augmentation import AugmentationPipeline, Cropper
+from df_extras import Flatten, Biternion
+
+
+def mknet():
+  return df.Sequential(                     # 220x76
+    df.SpatialConvolution( 3, 24, (3, 3)),  # 218x74
+    df.BatchNormalization(24),
+    df.ReLU(),
+    df.SpatialConvolution(24, 24, (3, 3)),  # 216x72
+    df.SpatialMaxPooling((3, 3)),           #  72x24
+    df.BatchNormalization(24),
+    df.ReLU(),
+    df.SpatialConvolution(24, 48, (3, 3)),  #  70x22
+    df.BatchNormalization(48),
+    df.ReLU(),
+    df.SpatialConvolution(48, 48, (3, 3)),  #  68x20
+    df.SpatialMaxPooling((2, 2)),           #  34x10
+    df.BatchNormalization(48),
+    df.ReLU(),
+    df.SpatialConvolution(48, 64, (3, 3)),  #  32x8
+    df.BatchNormalization(64),
+    df.ReLU(),
+    df.SpatialConvolution(64, 64, (3, 3)),  #  30x6
+    df.SpatialMaxPooling((2, 2)),           #  15x3
+    df.BatchNormalization(64),
+    df.ReLU(),
+    df.SpatialConvolution(64, 64, (3, 2)),  #  13x2
+    df.BatchNormalization(64),
+    df.ReLU(),
+    df.Dropout(0.2),
+    Flatten(),
+    df.Linear(64*13*2, 512),
+    df.ReLU(),
+    df.Dropout(0.5),
+    df.Linear(512, 2, init=df.init.normal(0.01)),
+    Biternion()
+  )
+
+
+def mkaug(Xtr, ytr):
+  return AugmentationPipeline(Xtr, ytr, Cropper((220,76)))
