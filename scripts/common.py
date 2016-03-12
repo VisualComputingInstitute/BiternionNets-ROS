49e5dadd45654b618332d4e726be18fc42749688
lucasb-eyer <lucasb.eyer.be@gmail.com>
2016-03-12 01:12:40 +0100

    Update to newest df2.

--- scripts/common.py
+++ scripts/common.py
@@ -4,25 +4,25 @@ from df_extras import Flatten, Biternion
 
 
 def mknet():
-  return df.Sequential(                   #     3@46
+  return df.Sequential(                     #     3@46
     df.SpatialConvolution( 3, 24, (3, 3)),  # -> 24@44
     df.BatchNormalization(24),
     df.ReLU(),
     df.SpatialConvolution(24, 24, (3, 3)),  # -> 24@42
     df.BatchNormalization(24),
-    df.SpatialMaxPooling((2, 2)),           # -> 24@21
+    df.SpatialMaxPooling((2, 2), ignore_border=False),  # -> 24@21
     df.ReLU(),
     df.SpatialConvolution(24, 48, (3, 3)),  # -> 48@19
     df.BatchNormalization(48),
     df.ReLU(),
     df.SpatialConvolution(48, 48, (3, 3)),  # -> 48@17
     df.BatchNormalization(48),
-    df.SpatialMaxPooling((2, 2)),           # -> 48@9
+    df.SpatialMaxPooling((2, 2), ignore_border=False),  # -> 48@9
     df.ReLU(),
-    df.SpatialConvolution(48, 64, (3, 3)),  # -> 48@7
+    df.SpatialConvolution(48, 64, (3, 3)),  # -> 64@7
     df.BatchNormalization(64),
     df.ReLU(),
-    df.SpatialConvolution(64, 64, (3, 3)),  # -> 48@5
+    df.SpatialConvolution(64, 64, (3, 3)),  # -> 64@5
     df.BatchNormalization(64),
     df.ReLU(),
     df.Dropout(0.2),
