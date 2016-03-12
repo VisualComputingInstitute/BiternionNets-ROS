1cb95275d4c27e766ce35cc5e18285719f44fb4e
lucasb-eyer <lucasb.eyer.be@gmail.com>
2016-03-12 01:55:49 +0100

    Major overhaul of re-labeling script.
    - Make use of `defaultdict` to simplify a lot.
    - Make a difference between labeldir and fromdir.

--- scripts/data_labeling.py
+++ scripts/data_labeling.py
@@ -1,81 +1,71 @@
+#!/usr/bin/env python
+
 from os import listdir, remove, makedirs
 from os.path import join, exists
 from shutil import copy2 as cp
+from sys import argv, exit
+from collections import defaultdict
 
-datadir = '/work/kurin/spencer_data/dump'
-new_dir = '/work/kurin/spencer_data/new_dump'
+if len(argv) != 4:
+    print("Usage: {} FROM_DIR LABELS_DIR TO_DIR".format(argv[0]))
+    print("")
+    print("FROM_DIR is the directory containing all images.")
+    print("LABELS_DIR needs to contain two sub-directories `4x` and `4p`.".format(argv[0]))
+    print("TO_DIR is the target directory within which new `4x` and `4p` folders are created.".format(argv[0]))
+    exit(1)
+
+datadir = argv[1]
+lbl_dir = argv[2]
+new_dir = argv[3]
 end = 'rgb.png'
 
-from_dict = {}
+print("Collection in progress...")
+
+from_dict = defaultdict(lambda: defaultdict(list))
 to_dict = {}
 
-
-#form from_dict
-f_d = datadir
-f_d = join(datadir,'50')
-for f in listdir(f_d):
-  if(not f.startswith('.')):
-    #p_d = {}
-    #for p in listdir(join(f_d,f)):
-    if not f.endswith('.png'):
-      continue
-    n,trid,pid,_ = f.rsplit('_')
-    curr = from_dict.get(n)
-    if curr is not None:
-      if curr.get(trid) is not None:
-        curr[trid].append(pid)
-      else:
-        from_dict[n][trid] = [pid]
-    else:
-      from_dict[n] = {trid:[pid]}
+# form from_dict, which contains all unlabelled source files.
+for f in listdir(datadir):
+  if not f.startswith('.') and f.endswith('.png'):
+    name,trid,pid,_ = f.rsplit('_')
+    from_dict[name][trid].append(pid)
 
 #form to_dict
-d = {'4p':['backleft','backright', 'frontleft', 'frontright'], '4x':['back', 'front', 'left', 'right']}
-to_dict = {'4p':{},'4x':{}}
+d = {'4p': ['backleft','backright', 'frontleft', 'frontright'],
+     '4x': ['back', 'front', 'left', 'right']}
+to_dict = {'4p': defaultdict(lambda: defaultdict(lambda: defaultdict(list))),
+           '4x': defaultdict(lambda: defaultdict(lambda: defaultdict(list)))}
 
 #create all dirs we need
 for k in d:
-  for el in d[k]:
-    path = join(new_dir, k, el)
+  for lbl in d[k]:
+    path = join(new_dir, k, lbl)
     if not exists(path):
       makedirs(path)
 
 for k in d:
-    for el in d[k]:
-        to_dict[k][el] = {}
-        p = join(datadir, k, el)
-        for f in listdir(p):
-            if(not f.startswith('.')):
+    for lbl in d[k]:
+        for f in listdir(join(lbl_dir, k, lbl)):
+            if not f.startswith('.'):
                 name,trid,pid,_ = f.rsplit('_')
-                if to_dict.get(k).get(el).get(name,0) == 0:
-                    to_dict[k][el][name] = {}
-                if to_dict.get(k).get(el).get(name).get(trid, 0) == 0:
-                    to_dict[k][el][name][trid] = [pid]
-                else:
-                    to_dict[k][el][name][trid].append(pid)
+                to_dict[k][lbl][name][trid].append(pid)
 
 #iterate in to_dict and copy pics from from_dict with the same name, tid and pid
 
-def crem(t,a,n,tid,pid,end):
-    f_name = join(datadir, '50', "_".join([n, tid, pid, end]))
-    t_name = join(new_dir, t, a, "_".join([n, tid, pid, end]))
-    #csvf_name = join(datadir, 'dumps', n, "_".join([tid, pid, 'd.csv']))
-    #csvt_name = join(new_dir, t, a, "_".join([n, tid, pid, 'd.csv']))
+def crem(t,lbl,name,tid,pid,end):
+    f_name = join(datadir, "_".join([name, tid, pid, end]))
+    t_name = join(new_dir, t, lbl, "_".join([name, tid, pid, end]))
     cp(f_name, t_name)
-    #cp(csvf_name, csvt_name)
-    #remove(f_name)
 
-print("Generating in progress...")
+print("Generation in progress...")
 n_im = 0
 for t in to_dict:
-    for a in to_dict[t]:
-        for n in to_dict[t][a]:
-            for tid in to_dict[t][a][n]:
-                pids = to_dict[t][a][n][tid]
-                if pids is not None:
-                  for p in pids:
-                    if p in from_dict[n].get(tid, []):
-                      crem(t,a,n,tid,p,end)
-                      n_im+=1
-print "Finished. Copied %s images" % (n_im)
+    for lbl in to_dict[t]:
+        for name in to_dict[t][lbl]:
+            for tid in to_dict[t][lbl][name]:
+                for p in to_dict[t][lbl][name][tid]:
+                    if p in from_dict[name][tid]:
+                        crem(t,lbl,name,tid,p,end)
+                        n_im+=1
 
+print "Finished. Copied %s images" % (n_im)
